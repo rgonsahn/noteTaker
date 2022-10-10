d347c4b36634d597e82d2e3efcd2a064cb896d72
rgonsahn <rgonsahn@yahoo.com>
2022-10-09 22:26:18 -0400

    added notes path

---
+++ server.js
@@ -0,0 +1,49 @@
+const express= require("express");
+const app= express();
+const port= 3001;
+const path= require("path")
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/notes.html"))   
+})
+
+app.get()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>console.log("listening on:"+ port))
