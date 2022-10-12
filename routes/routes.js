04dc4d3b0161218747afe2eb9fee7898504142e1
rgonsahn <rgonsahn@yahoo.com>
2022-10-12 00:48:15 -0400

    created new server.js file

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const fs= require('fs');
-const path= require('path');
-
-module.exports= app =>{
-    fs.readFile("db/db.json","utf-8",(err,data)=>{
-        if (err) throw err;
-        var myNotes= json.parse(data);
-    })
-}
--- server.js
+++ server.js
@@ -1,49 +1,19 @@
-const express= require("express");
-const app= express();
-const port= 3001;
-const path= require("path")
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3001;
+const path = require("path")
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-res.sendFile(path.join(__dirname,"./public/index.html"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 
-app.get("/notes",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./public/notes.html"))   
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
-app.get()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port,()=>console.log("listening on:"+ port))
+app.listen(port, () => console.log("listening on:" + port))
