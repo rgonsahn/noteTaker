33c1ddf3f575394ee0b9b2bce35aa9638ad8ecbe
rgonsahn <rgonsahn@yahoo.com>
2022-10-13 13:57:55 -0400

    created response.json notes route

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const fs = require("fs");
 const app = express();
 const port = 3001;
 const path = require("path")
-
+const util = require("util")
+const writeFile= util.promisify(fs.writeFile)
+const readFile= util.promisify(fs.readFile)
+function getNotes(){
+    return readFile("db/db.json","utf-8").then(rawNotes =>{
+        return JSON.parse(rawNotes)
+    })
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -15,5 +22,19 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
-
+app.get("/api/notes",(req,res)=> {
+getNotes().then(notes => res.json(notes))
+})
+app.post("/api/notes",(req,res)=>{
+    getNotes().then(oldNotes =>{
+        var updatedNotes=[...oldNotes,{
+            title: req.body.title,
+            text: req.body.text,
+            id: "1234"
+        }]
+       writeFile("db/db.json",JSON.stringify(updatedNotes))
+    }).then(()=>res.json({
+        msg:"ok"
+    }))
+})
 app.listen(port, () => console.log("listening on:" + port))
